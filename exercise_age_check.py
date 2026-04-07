b3a762bffc0616f0af86ac560475c27d24f5564a
emiliakoron <ekoron@mail.austral.edu.ar>
2026-04-06 23:28:50 -0300

    Update exercise_age_check.py

--- exercise_age_check.py
+++ exercise_age_check.py
@@ -19,3 +19,12 @@ def age_check():
         Entrada invalida
     """
     pass
+    edad = int(input())
+    limite_edad = int(input())
+    if edad <= 0 or limite_edad <= 0:
+        print("Entrada invalida")
+    elif edad >= limite_edad:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
+        
