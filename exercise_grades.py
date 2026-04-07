ef167217cc5528cfa3e6fd420e97eb274185a2fa
emiliakoron <ekoron@mail.austral.edu.ar>
2026-04-06 23:53:57 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -19,3 +19,12 @@ def grades():
         Insuficiente
     """
     pass
+    nota = int(input())
+    if nota >= 9 and nota <=10:
+        print("Excelente")
+    elif nota >= 7 and nota <=8:
+        print("Bueno")
+    elif nota >= 5 and nota <=6:
+        print("Regular")
+    elif nota >= 0 and nota <=4:
+        print("Insuficiente")
